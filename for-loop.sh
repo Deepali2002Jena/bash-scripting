b4af820883447506ce14d2c8a5f6228b8a5249c2
Deepali Jena <jenadeepali47@gmail.com>
2025-03-21 17:59:22 +0530

    Create for-loop.sh

---
+++ for-loop.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+for i in {0..20..2}
+do
+	echo "Even numbers are : $i"
+done
+
+###############################################
+
+for (( i=1; i<=5; i++ ))
+do
+    echo "Iteration: $i"
+done
+
